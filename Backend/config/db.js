c57394889d8cb0979238df1845fa22a206ee34e4
Ankit511mishra <149512364+Ankit511mishra@users.noreply.github.com>
2024-07-27 18:06:10 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri =
-    "mongodb+srv://<username>:<password>@cluster0.6kzwkfo.mongodb.net/<dbName>?retryWrites=true&w=majority&appName=Cluster0";
-
+    "mongodb+srv://ankit:<password>@cluster0.ado926m.mongodb.net/<dbName>?retryWrites=true&w=majority&appName=Cluster0"
 let dbURL = uri.replace("<username>", process.env.DB_USERNAME);
 dbURL = dbURL.replace("<password>", process.env.DB_PASSWORD);
 dbURL = dbURL.replace("<dbName>", process.env.DB_NAME);
